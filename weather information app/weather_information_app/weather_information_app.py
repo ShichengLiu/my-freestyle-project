b67cd4941ace3f2645effff4e41c7f4faef766c1
ShichengLiu <36935504+ShichengLiu@users.noreply.github.com>
2018-06-25 17:27:32 -0400

    Change the output result
    Give a better output.

--- weather information app/weather_information_app/weather_information_app.py
+++ weather information app/weather_information_app/weather_information_app.py
@@ -62,10 +62,9 @@ if weather["icon"] == "rain":
     print("..." + "Take an Umbrella")
 else:
     pass
-
-print("Have a Good Day!")
 if float(weather["temperature"]) > 70:
     print("..." + " Wear Your T-Shirt And Shorts")
 else:
     print("..." + "Wear Your Long Sleeves And Pants")
+print("Have a Good Day!")
 print("-------------------------------------")
